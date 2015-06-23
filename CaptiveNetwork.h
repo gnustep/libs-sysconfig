f4fe65e2270e711e9c3b1a84c434667fbf0b80a1
Riccardo Mottola <rm@gnu.org>
2015-06-23 23:37:18 +0000

    systemconfiguration framework, first stub
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/devmodules/dev-libs/sysconfig@38675 72102866-910b-0410-8b05-ffd578937521

---
+++ CaptiveNetwork.h
@@ -0,0 +1,38 @@
+/*
+   Project: SystemConfiguration
+
+   Copyright (C) 2015 Free Software Foundation
+
+   Author: Riccardo Mottola
+
+   Created: 2015-06-19 15:23:26 +0000 by multix
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+*/
+
+#ifndef _CAPTIVENETWORK_H_
+#define _CAPTIVENETWORK_H_
+
+#import <Foundation/Foundation.h>
+
+@interface CaptiveNetwork : NSObject
+{
+
+}
+
+@end
+
+#endif // _CAPTIVENETWORK_H_
+
